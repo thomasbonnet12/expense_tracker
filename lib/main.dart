0f446703966d9a86e3a496f58ace75514ba8e8df
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-20 22:35:28 +0100

    styling applied to card

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,39 @@ class MyHomePage extends StatelessWidget {
             ),
             Column(
               children: transactions.map((tx) {
-                return Card(
-                  child: Text(tx.title),
+                return Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey, fontSize: 13),
+                        ),
+                      ],
+                    )
+                  ],
                 );
               }).toList(),
             )
