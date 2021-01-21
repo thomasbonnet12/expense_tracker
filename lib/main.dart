3a3889a66ca42d8df86131c67254f5a4e0e90d19
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-21 21:16:30 +0100

    intl package installed and dateformat added to have a better date format in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -54,7 +55,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -71,7 +72,7 @@ class MyHomePage extends StatelessWidget {
                               fontWeight: FontWeight.bold, fontSize: 17),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat('dd-MM-yyyy').format(tx.date),
                           style: TextStyle(color: Colors.grey, fontSize: 13),
                         ),
                       ],
