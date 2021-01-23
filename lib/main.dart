15c6ff6f80f18ed9d1fcb17d1e5649edb217eeba
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-23 14:46:49 +0100

    startaddnewtransaction function added into main.dart to obtain a bottomsheet shoing dynamically when we push the icon plus, the file user transaction has been deleted because all the logics have benn trasfered to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:expense_tracker_app/widget/user_transaction.dart';
+import 'package:expense_tracker_app/models/transaction.dart';
+import 'package:expense_tracker_app/widget/new_transaction.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import './models/transaction.dart';
+import './widget/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,9 +23,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  // String titleInput;
-  // String amountInput;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 'T1', title: "New T-shirt", amount: 71.80, date: DateTime.now()),
+    Transaction(
+        id: 'T2', title: "New Shoes", amount: 44.80, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +71,7 @@ class MyHomePage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           )
         ],
       ),
@@ -47,14 +87,14 @@ class MyHomePage extends StatelessWidget {
                 child: Text('data'),
               ),
             ),
-            UserTransaction(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
