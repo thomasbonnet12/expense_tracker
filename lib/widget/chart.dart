eccae2ec22fc08fc97a95704a047e3f1a3a29e58
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-23 19:25:43 +0100

    Stack created in chat_bar

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -25,7 +25,7 @@ class Chart extends StatelessWidget {
       print(totalSum);
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': 9.99,
       };
     });
@@ -38,7 +38,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']} : ${data['amount']}');
+        }).toList(),
       ),
     );
   }
