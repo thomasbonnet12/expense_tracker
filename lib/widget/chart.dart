11d083aca8decb0d76867c3d2132443568110140
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-24 13:01:40 +0100

    padding, fittedbox and flexible added in chart.dart and chart_bar.dart to avoid big number to make the charts look ugly

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -44,15 +44,22 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return ChartBar(
-              data['day'],
-              data['amount'],
-              maxSpending == 0.0
-                  ? 0.0
-                  : (data['amount'] as double) / maxSpending);
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  maxSpending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / maxSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
